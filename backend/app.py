eefe70822aa44731ac7726a19ff5ef7da8887d81
yhh12345678 <yuhaoze25@mails.ucas.ac.cn>
2026-04-14 19:59:36 +0800

    增加一部分app.route()的用法

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,40 @@
 from flask import Flask
+from markupsafe import escape
+
 app = Flask(__name__)
+
+# app.route()的常见用法
+
 @app.route('/')
-def hello():
-    return 'Hello, Flask in VS Code!'
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
 if __name__ == '__main__':
     app.run(debug=True)
