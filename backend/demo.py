c5dd5f2d7b31a7291042ec585c386231fc7fbc38
yhh12345678 <yuhaoze25@mails.ucas.ac.cn>
2026-04-17 02:00:47 +0800

    modified:   backend/demo.py 在后端实现模型调用

--- backend/demo.py
+++ backend/demo.py
@@ -1,12 +1,19 @@
-from ollama import chat
-from ollama import ChatResponse
+import requests
+import json
 
-response: ChatResponse = chat(model='qwen3.5:0.8b', messages=[
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
-])
-print(response['message']['content'])
-# or access fields directly from the response object
-print(response.message.content)
+
+data = {
+    "model": "qwen3.5:0.8b",
+    "prompt": "hello",
+    "stream": False
+}
+
+
+response = requests.post(
+    "http://localhost:11434/api/generate",
+    json=data          
+)
+
+
+print(response.status_code)
+print(response.json()["response"])   
